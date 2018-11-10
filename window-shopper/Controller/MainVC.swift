63fb0fc5b33df6f01ecfe4f5114d4ef89ee69563
gdm <gdm.mbp@gdmMBP.local>
2018-11-10 00:11:44 -0500

    Second commit. Add text field and button

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by gdm on 11/9/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageText: CurrencyTextField!
+    @IBOutlet weak var priceText: CurrencyTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.3424063743, blue: 0.02462279602, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageText.inputAccessoryView = calcButton
+        priceText.inputAccessoryView = calcButton
+        
+    }
+
+    @objc func calculate() {
+        print("We got here")
+    }
+}
+
